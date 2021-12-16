fdbfe84a7455ed827b769b15a50a69c490d2bb3d
erocha <edsonrocha@egnetech.com.br>
2021-12-16 16:05:13 -0300

    GPS show/hide moving markers

---
+++ qzela/Helpers/NetworkListener.swift
@@ -0,0 +1,67 @@
+import Network
+
+public enum ConnectionType {
+    case wifi
+    case ethernet
+    case cellular
+    case unknown
+}
+
+class NetworkListener {
+    
+    weak var delegate: NetworkListenerDelegate? = nil
+    
+    //    static public let shared = NetworkListener()
+    var monitor: NWPathMonitor
+    var path: NWPath?
+    lazy var pathUpdateHandler: (NWPath) -> Void = { path in
+        self.path = path
+        if path.status == NWPath.Status.satisfied {
+            print("Connected")
+        } else if path.status == NWPath.Status.unsatisfied {
+            print("unsatisfied")
+        } else if path.status == NWPath.Status.requiresConnection {
+            print("requiresConnection")
+        }
+    }
+    var connType: ConnectionType = .wifi
+    
+    init() {
+        monitor = NWPathMonitor()
+//        monitor.pathUpdateHandler = pathUpdateHandler
+        monitor.pathUpdateHandler = { path in
+            self.checkConnectionTypeForPath(path)
+        }
+        self.monitor.start(queue: DispatchQueue.global(qos: .background))
+    }
+    
+
+    func isNetworkAvailable() -> Bool {
+        self.checkConnectionTypeForPath(monitor.currentPath)
+        if monitor.currentPath.status == NWPath.Status.satisfied {
+            return true
+        }
+        return false
+    }
+    
+    func checkConnectionTypeForPath(_ path: NWPath) {
+        
+        var networkStatus: ConnectionType = .unknown
+        if monitor.currentPath.status == NWPath.Status.satisfied {
+            if path.usesInterfaceType(.wifi) {
+                networkStatus = .wifi
+            } else if path.usesInterfaceType(.wiredEthernet) {
+                networkStatus = .ethernet
+            } else if path.usesInterfaceType(.cellular) {
+                networkStatus = .cellular
+            }
+        }
+
+        self.delegate?.networkListenerStatus(status: networkStatus)
+        
+    }
+}
+
+protocol NetworkListenerDelegate: AnyObject {
+    func networkListenerStatus(status: ConnectionType)
+}
