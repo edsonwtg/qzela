c721dad20b9e8651a2c361c84676de3900780e07
erocha <edsonrocha@egnetech.com.br>
2021-12-23 16:59:16 -0300

    Card view

---
+++ qzela/Helpers/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  qzela
+//
+//  Created by Edson Rocha on 23/12/21.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mywebSeriesImage: UIImageView!
+    
+}
--- qzela/View Controllers/PopUpMarkerViewController.swift
+++ qzela/View Controllers/PopUpMarkerViewController.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
-class PopUpMarkerViewController: UIViewController {
+class PopUpMarkerViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return webSereiesImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        cell.mywebSeriesImage.image=UIImage(named: webSereiesImages[indexPath.row])
+        cell.mywebSeriesImage.layer.cornerRadius=50.0
+        cell.layoutIfNeeded()
+
+        return cell
+    }
+    
+    
+    var webSereiesImages: [String] = ["img_open_0", "img_open_1", "map"]
+
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
 
     @IBAction func btClose(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -20,3 +38,22 @@ class PopUpMarkerViewController: UIViewController {
     
 }
 
+//extension PopUpMarkerViewController: UICollectionViewDelegateFlowLayout {
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let size = sliderCollectionView.frame.size
+//        return CGSize(width: size.width, height: size.height)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
+//    }
+//}
