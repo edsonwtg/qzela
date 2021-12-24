fa6ebccaef76e74a1ad6f9813a75081ccb79f8b2
erocha <edsonrocha@egnetech.com.br>
2021-12-24 09:09:29 -0300

    Collection view image incidents

--- qzela/View Controllers/PopUpMarkerViewController.swift
+++ qzela/View Controllers/PopUpMarkerViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class PopUpMarkerViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+    var webSereiesImages: [String] = ["img_open_0", "img_open_1", "map"]
+    
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBAction func btClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageControl.numberOfPages = webSereiesImages.count
+
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return webSereiesImages.count
     }
@@ -17,43 +33,40 @@ class PopUpMarkerViewController: UIViewController, UICollectionViewDelegate, UIC
         let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
         cell.mywebSeriesImage.image=UIImage(named: webSereiesImages[indexPath.row])
         cell.mywebSeriesImage.layer.cornerRadius=50.0
-        cell.layoutIfNeeded()
 
         return cell
     }
     
-    
-    var webSereiesImages: [String] = ["img_open_0", "img_open_1", "map"]
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+       let witdh = scrollView.frame.width - (scrollView.contentInset.left*2)
+       let index = scrollView.contentOffset.x / witdh
+       let roundedIndex = round(index)
+       self.pageControl.currentPage = Int(roundedIndex)
+   }
 
-    @IBOutlet weak var sliderCollectionView: UICollectionView!
-
-    @IBAction func btClose(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
 }
+    
 
-//extension PopUpMarkerViewController: UICollectionViewDelegateFlowLayout {
-//    
+extension PopUpMarkerViewController: UICollectionViewDelegateFlowLayout {
+    
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
 //        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 //    }
-//    
+    
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        let size = sliderCollectionView.frame.size
-//        return CGSize(width: size.width, height: size.height)
+//        print("size.width: \(size.width) size.height: \(size.height)")
+//        return CGSize(width: size.width-20, height: size.height)
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
 //        return 0.0
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0.0
+//        return 20.0
 //    }
-//}
+}
