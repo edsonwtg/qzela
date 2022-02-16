33df249ee90fbc7a57c7db091214113013e02fd4
erocha <edsonrocha@egnetech.com.br>
2022-02-16 16:31:24 -0300

    reclassified Directories Move files to Directory

---
+++ qzela/Segment Fragment/SegmentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentViewController.swift
+//  qzela
+//
+//  Created by Edson Rocha on 16/02/22.
+//
+
+import UIKit
+
+class SegmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+}
